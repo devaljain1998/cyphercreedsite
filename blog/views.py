a660b7fbf7af94e1f153b6a95768cb2af19d811f
devaljain1998 <devaljain1998@gmail.com>
2019-02-13 23:42:00 +0530

    Admin's Blog Completed

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    title = "Admin's Blog"
+    return render(request,'blog/post_list.html',{'posts':posts,'title':title})
